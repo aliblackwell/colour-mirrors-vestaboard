e2f5728e704f1ac042a2f80447aa3bd6fe562f7c
Alasdair Blackwell <aliblackwell@protonmail.com>
2022-02-16 15:55:33 +0000

    Send bottle every day using node-cron

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import express from "express";
 import helmet from "helmet";
 import fetch from "node-fetch";
+import cron from "node-cron";
 import bottles from "./bottles.js";
 
 const SERVICE_PORT = process.env.SERVICE_PORT || 4000;
@@ -15,27 +16,44 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 
-app.get("/send-bottle", function (req, res) {
+cron.schedule("30 10 * * *", () => {
   sendBottle();
-  res.send("success");
 });
 
-function sendBottle() {
+app.get("/send-bottle", async (req, res) => {
+  try {
+    const response = await sendBottle();
+    const data = await response.json();
+    if (data.message) {
+      res.sendStatus(200);
+    } else {
+      respondWithError(res, "Message failed to send");
+    }
+  } catch (e) {
+    console.log(e);
+    respondWithError(res, "Vestaboard fetch failed");
+  }
+});
+
+const sendBottle = async () => {
   let bottle = bottles[Math.floor(Math.random() * bottles.length)];
-  // fetch(
-  //   `https://platform.vestaboard.com/subscriptions/${process.env.VESTABOARD_DEFAULT_SUBSCRIPTION}/message`,
-  //   {
-  //     body: `{"text":"Today's bottle is ${bottle.bottle_id}: ${bottle.name} (${bottle.top} / ${bottle.bottom})"}`,
-  //     headers: {
-  //       "Content-Type": "application/x-www-form-urlencoded",
-  //       "X-Vestaboard-Api-Key": `${process.env.VESTABOARD_API_KEY}`,
-  //       "X-Vestaboard-Api-Secret": `${process.env.VESTABOARD_API_SECRET}`,
-  //     },
-  //     method: "POST",
-  //   }
-  // );
-  console.log(bottle)
-}
+  return await fetch(
+    `https://platform.vestaboard.com/subscriptions/${process.env.VESTABOARD_DEFAULT_SUBSCRIPTION}/message`,
+    {
+      body: `{"text":"Today's bottle is ${bottle.bottle_id}: ${bottle.name} (${bottle.top} / ${bottle.bottom})"}`,
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "X-Vestaboard-Api-Key": `${process.env.VESTABOARD_API_KEY}`,
+        "X-Vestaboard-Api-Secret": `${process.env.VESTABOARD_API_SECRET}`,
+      },
+      method: "POST",
+    }
+  );
+};
+
+const respondWithError = (res, errorMessage) => {
+  res.status(500).send({ error: errorMessage });
+};
 
 app.listen(SERVICE_PORT, SERVICE_HOST);
 
